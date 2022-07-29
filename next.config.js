099cee3bee386b97b039528b87719f7fcb67fb6c
Flavio Ribeiro <flavrance@gmail.com>
2022-07-29 13:20:38 -0300

    modificado next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images.unoptimized: true
+  swcMinify: true, 
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },  
+
 }
+
 
 module.exports = nextConfig
